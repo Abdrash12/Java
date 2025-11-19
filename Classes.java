b42ec6b66e1d631313a399dffc78084de47fbeaa
Rashid <rsmaabdullah1@gmail.com>
2025-11-19 19:28:25 +0500

    Add files via upload
    Java OOP Basics submission covering:
    
    Classes & Objects: Defining Car and Math classes and creating instances.
    
    Constructors: Initializing objects with specific attributes (e.g., model, color, numbers).
    
    Methods: Implementing behaviors like start() for Car and multiply() for Math operations.

---
+++ Classes.java
@@ -0,0 +1,47 @@
+public class Classes {
+    public static void main(String[] args) {
+        int num =5;
+        String name ="Meow";
+        double nums = 3.14;
+        boolean hmm = true;
+        System.out.println("hello world"+num);
+        Car car1 = new Car(1981,"pink");
+        Math math1 = new Math(8,9);
+        math1.multiply();
+        car1.start();
+        car1.model();        
+    }
+}
+    class Car {
+        int model ;
+        String color ;
+
+        Car(int model,String color){
+            this.model = model;
+            this.color = color;
+        }
+        void model(){
+            System.out.println("The model of the vehicle is: "+model);
+        }
+    void start(){
+        System.out.println("vroom vroom!!");
+    }
+
+    
+}
+     class Math {
+        int a;
+        int b;
+        Math(int a,int b)
+        {
+            this.a = a;
+            this.b =b;
+        }
+        void multiply()
+        {
+            System.out.println("The answer for "+a+"X"+b+"="+(a*b));
+        }
+    
+        
+    }
+    
